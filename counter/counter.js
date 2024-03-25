5d0bc566e3cd44b74e300eb0c38e3cf55706f48b
SirajMia <sirajulshuvo08@gmail.com>
2024-03-25 19:51:13 +0600

    add some modern js mathod

--- counter/counter.js
+++ counter/counter.js
@@ -1,26 +1,30 @@
 function counter(){
-    let increase = document.getElementById('increase');
-    let decrease = document.getElementById('decrease');
-    let myInput = document.getElementById('myInput');
+    let output = document.getElementById('output');
+    let myBtnAll = document.querySelectorAll('button');
     let count = 0;
-    increase.addEventListener('click',(e) => {
-        count++;
-        myInput.value = count;
-        updatedColor()
+
+    myBtnAll.forEach((value) => {
+        value.addEventListener('click',(e) =>{
+            let inner = e.target.innerHTML;
+            if(inner === '+'){
+                count++;
+                output.innerHTML = count;
+            }else if(inner === '-'){
+                if(count > 0){
+                    count--;
+                    output.innerHTML = count;
+                }
+            }
+            if(count <= 10){
+                output.style.color = 'green'; 
+                output.style.fontWeight = 'bold'
+            }
+            if(count >= 10){
+                output.style.color = 'darkblue';
+                output.style.fontWeight = 'bold'
+            }
+        })
     })
-    decrease.addEventListener('click',(e) => {
-        count--;
-        myInput.value = count;
-        updatedColor()
-    })
-    function updatedColor() {
-        if(count < 0){
-          myInput.style.color = 'red'
-        }
-        if(count > 0){
-            myInput.style.color = 'black'
-        }
-    }
 
 }
 counter()
