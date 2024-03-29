907b782b1fa3a967e68a0ce3315eb69906d1ee46
SirajMia <sirajulshuvo08@gmail.com>
2024-03-29 19:09:48 +0600

    add unitconverterproject

---
+++ unitconverter/unit.js
@@ -0,0 +1,16 @@
+let feet = document.getElementById('feet');
+let inch = document.getElementById('inch');
+
+feet.addEventListener('input', (e) => {
+    let f = parseFloat(feet.value);
+    inch.value = (f * 12).toFixed();
+});
+
+inch.addEventListener('input', (e) => {
+    let i = parseFloat(inch.value);
+    if(i % 2 === 1){
+        feet.value = (i/12).toFixed(2);
+    }else{
+        feet.value = (i / 12);
+    }
+});
