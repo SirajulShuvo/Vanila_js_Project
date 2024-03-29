8b3f24af56a5bef39d9568f9be519c0514d4bafd
SirajMia <sirajulshuvo08@gmail.com>
2024-03-29 19:11:11 +0600

    add function

--- unitconverter/unit.js
+++ unitconverter/unit.js
@@ -1,16 +1,19 @@
-let feet = document.getElementById('feet');
-let inch = document.getElementById('inch');
+function unitConverter() {
+    let feet = document.getElementById('feet');
+    let inch = document.getElementById('inch');
 
-feet.addEventListener('input', (e) => {
-    let f = parseFloat(feet.value);
-    inch.value = (f * 12).toFixed();
-});
+    feet.addEventListener('input', (e) => {
+        let f = parseFloat(feet.value);
+        inch.value = (f * 12).toFixed();
+    });
 
-inch.addEventListener('input', (e) => {
-    let i = parseFloat(inch.value);
-    if(i % 2 === 1){
-        feet.value = (i/12).toFixed(2);
-    }else{
-        feet.value = (i / 12);
-    }
-});
+    inch.addEventListener('input', (e) => {
+        let i = parseFloat(inch.value);
+        if(i % 2 === 1){
+            feet.value = (i/12).toFixed(2);
+        }else{
+            feet.value = (i / 12);
+        }
+    });
+}
+unitConverter();
