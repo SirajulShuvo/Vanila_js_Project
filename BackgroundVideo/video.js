ffc9f77b2b65d2353639de8ec50a9b02d71bb16b
SirajMia <sirajulshuvo08@gmail.com>
2024-04-11 14:51:59 +0600

    vidoe background with play and pause

---
+++ BackgroundVideo/video.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.switch-btn')
+const video = document.querySelector('.video-container')
+btn.addEventListener('click',(e) =>{
+    if(!btn.classList.contains('slide')){
+        btn.classList.add('slide');
+        video.pause();
+    }else{
+        btn.classList.remove('slide');
+        video.play();
+    }
+})
